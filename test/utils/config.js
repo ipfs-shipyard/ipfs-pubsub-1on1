5265c725e2c94e512a57c04be9d5d4b8b37ee339
haad <haad@headbanggames.com>
2018-02-01 20:14:30 +0100

    Remove obsolete code, update package-lock

--- test/utils/config.js
+++ test/utils/config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  timeout: 60000,
   defaultIpfsConfig: {
     start: true,
     EXPERIMENTAL: {
@@ -22,77 +21,5 @@ module.exports = {
         }
       },
     }
-  },
-  daemon1: {
-    repo: './ipfs/pubsub-direct-channel/tests/daemon1',
-    start: true,
-    EXPERIMENTAL: {
-      pubsub: true
-    },
-    config: {
-      Addresses: {
-        API: '/ip4/127.0.0.1/tcp/0',
-        Swarm: ['/ip4/0.0.0.0/tcp/0'],
-        Gateway: '/ip4/0.0.0.0/tcp/0'
-      },
-      Bootstrap: [],
-      Discovery: {
-        MDNS: {
-          Enabled: true,
-          Interval: 1
-        },
-        webRTCStar: {
-          Enabled: false
-        }
-      },
-    },
-  },
-  daemon2: {
-    repo: './ipfs/pubsub-direct-channel/tests/daemon2',
-    start: true,
-    EXPERIMENTAL: {
-      pubsub: true
-    },
-    config: {
-      Addresses: {
-        API: '/ip4/127.0.0.1/tcp/0',
-        Swarm: ['/ip4/0.0.0.0/tcp/0'],
-        Gateway: '/ip4/0.0.0.0/tcp/0'
-      },
-      Bootstrap: [],
-      Discovery: {
-        MDNS: {
-          Enabled: true,
-          Interval: 1
-        },
-        webRTCStar: {
-          Enabled: false
-        }
-      },
-    },
-  },
-  daemon3: {
-    repo: './ipfs/pubsub-direct-channel/tests/daemon3',
-    start: true,
-    EXPERIMENTAL: {
-      pubsub: true
-    },
-    config: {
-      Addresses: {
-        API: '/ip4/127.0.0.1/tcp/0',
-        Swarm: ['/ip4/0.0.0.0/tcp/0'],
-        Gateway: '/ip4/0.0.0.0/tcp/0'
-      },
-      Bootstrap: [],
-      Discovery: {
-        MDNS: {
-          Enabled: true,
-          Interval: 1
-        },
-        webRTCStar: {
-          Enabled: false
-        }
-      },
-    },
   },
 }
